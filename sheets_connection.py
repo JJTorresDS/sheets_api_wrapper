0f0c707c90a41fa7865072f50affe6bc1d55242d
Jonas Torres <jonastorres@macbook-pro-de-jonas.tail0ffc4.ts.net>
2024-02-07 11:38:37 -0300

    Added CREDENTIALS as a variable to pass to the function

--- sheets_connection.py
+++ sheets_connection.py
@@ -35,7 +35,7 @@ def main(SAMPLE_SPREADSHEET_ID, CREDENTIALS,
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
+          CREDENTIALS, SCOPES
       )
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
